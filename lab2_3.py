78c2aa4339558bc2ee67d7c9c53e7d9869740e7d
Bas van den Heuvel <bas.vandenheuvel@student.uva.nl>
2015-04-09 12:30:47 +0200

    Plots

---
+++ lab2_3.py
@@ -0,0 +1,28 @@
+from scipy.stats import norm
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+def main():
+    rv = norm()
+    x = np.linspace(rv.ppf(0.001), rv.ppf(0.999), 100)
+
+    pdf = rv.pdf(x)
+    cdf = rv.cdf(x)
+
+    r = rv.rvs(size=1000)
+
+    mpl.rcParams['text.usetex'] = True
+    mpl.rcParams['text.latex.unicode'] = True
+
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(x, pdf, label=r'$\phi\text{(pdf)}$')
+    ax.plot(x, cdf, label=r'$\Phi\text{(cdf)}$')
+    ax.hist(r, bins=30, normed=True, histtype='stepfilled', alpha=0.2)
+    ax.legend(loc='best', frameon=True)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
