599f4e8c7ceff57d4f7775cfc7bac25fb9e0175d
Bas van den Heuvel <bas.vandenheuvel@student.uva.nl>
2015-04-21 13:04:42 +0200

    Confidence python code

---
+++ 3/confidence.py
@@ -0,0 +1,51 @@
+from random import sample
+from numpy import std
+from math import sqrt
+
+
+TEST_CASE_N = 10000
+TEST_DATA_N = 50
+C = 2.009
+ALPHA = 0.05
+
+
+def main():
+    with open('tijden-medium.log', 'r') as f:
+        data = []
+
+        for line in f:
+            data.append(float(line.rstrip('\n')))
+
+        confidence(data)
+
+
+def confidence(data):
+    total_mu = sum(data) / len(data)
+
+    hits = 0
+
+    for i in range(TEST_CASE_N):
+        if test_case(data, total_mu):
+            hits += 1
+
+    print('Estimated chance: %f' % (1-ALPHA))
+    print('Hits: %d, misses: %d, calculated chance: %f' % (hits,
+                                                           TEST_CASE_N-hits,
+                                                           hits/TEST_CASE_N))
+
+
+def test_case(data, mu):
+    test_data = sample(data, TEST_DATA_N)
+
+    x_avg = sum(test_data) / TEST_DATA_N
+    s = std(test_data)
+
+    t = (x_avg - mu) / (s / sqrt(TEST_DATA_N))
+
+    # print('c: %f, T: %f' % (C, t))
+
+    return t >= -C and t <= C
+
+
+if __name__ == '__main__':
+    main()
