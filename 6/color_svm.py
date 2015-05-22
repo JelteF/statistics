330068b6993bb15874debd9db531f3315c890f97
Bas van den Heuvel <bas.vandenheuvel@student.uva.nl>
2015-05-22 20:08:35 +0200

    Experiment

--- 6/color_svm.py
+++ 6/color_svm.py
@@ -1,14 +1,16 @@
 from sklearn import preprocessing
 from sklearn import svm
 from sklearn import grid_search
+from itertools import combinations_with_replacement
+from functools import partial
+import time
 import numpy as np
 import numpy.random as rd
 
 
-def main(seed=None):
-    colors = ['black', 'blue', 'brown', 'gray',
-              'green', 'orange', 'pink', 'red',
-              'violet', 'white', 'yellow']
+def main(seed=None, with_combinations=False):
+    colors = ['black', 'blue', 'brown', 'gray', 'green', 'orange', 'pink',
+              'red', 'violet', 'white', 'yellow']
 
     # does a line of text contains a color name?
     def containsColor(line):
@@ -19,7 +21,7 @@ def main(seed=None):
 
     # read the file and store spectra in matrix D (rows are the spectra)
     # and the classes in vector y
-    fp = open("data/natural400_700_5.asc", encoding='utf-8')
+    fp = open('data/natural400_700_5.asc', encoding='utf-8')
     X = np.zeros((0, 61))
     y = np.array([])
 
@@ -27,18 +29,24 @@ def main(seed=None):
     # http://stackoverflow.com/a/2990873/2570866
     line_iterator = iter(fp)
 
+    # Read data, using two lines per datapoint: the first for the class vector
+    # y, and the second for matrix X.
     for comment, vec in zip(line_iterator, line_iterator):
         ind = containsColor(comment)
         if ind is not None:
-            d = np.fromstring(vec, dtype=int, sep=" ")
+            d = np.fromstring(vec, dtype=int, sep=' ')
             X = np.append(X, np.array([d]), axis=0)
             y = np.append(y, ind)
 
-    # Split data into a matrix X containing all RVs, and y, containing all
-    # classes.
+    if with_combinations:
+        n = X.shape[0]
+        for i, j in combinations_with_replacement(range(61), 2):
+            a = X[:, i] * X[:, j]
+            X = np.hstack((X, a.reshape((n, 1))))
 
     # Scale data.
     X_scaled = preprocessing.scale(X)
+    # print(X_scaled)
 
     # Choose learn and test data.
     if seed is not None:
@@ -54,15 +62,12 @@ def main(seed=None):
 
     # Create SVM.
     clf = svm.SVC()
-    print(clf.get_params())
 
     # Brute-force parameters
     params = [{'C': [1, 10, 100, 1000], 'kernel': ['linear']},
               {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001],
                'kernel': ['rbf']}, ]
     clf_p = grid_search.GridSearchCV(clf, params)
-    print(clf)
-    print(clf_p)
 
     # Fit data.
     clf.fit(X_learn, y_learn)
@@ -88,9 +93,10 @@ def main(seed=None):
         cm_p[cls, pred_cls_p] += 1
 
     print('%d/%d (%f%%) good' % (good, len(y_test), good / len(y_test) * 100))
+    print(cm)
     print('%d/%d (%f%%) good (p)' % (good_p, len(y_test),
                                      good_p / len(y_test) * 100))
-    print(cm)
+    print(cm_p)
 
 
 def cnvt(s):
@@ -103,5 +109,7 @@ def cnvt(s):
 
 
 if __name__ == '__main__':
-    # main(seed=1)
-    main()
+    p_main = partial(main, seed=int(time.time()))
+    print(p_main)
+    p_main()
+    p_main(with_combinations=True)
